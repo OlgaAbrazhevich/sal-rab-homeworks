748866fd817bd4f7049a86476b09c44182cc69e6
Olga Abrazhevich <olga.abrazhevich@gmail.com>
2022-10-24 17:07:11 +0300

    homework2

--- task_2/solution.js
+++ task_2/solution.js
@@ -5,7 +5,7 @@ function calcShipping(sum, min, shipping) {
     let shippingSum;
     if(productsSum == 0 || productsSum >= freeShippingMinSum) {
         shippingSum = 0;
-    } else if ((productsSum > 0) & (productsSum < freeShippingMinSum)) {
+    } else if ((productsSum > 0) && (productsSum < freeShippingMinSum)) {
         shippingSum = shippingPrice;
     }
 
